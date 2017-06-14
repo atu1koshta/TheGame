cd8586ead69d452a2d0bb08e30dc7aa8664846ce
challengeratul <atulk2018@gmail.com>
2017-06-14 23:40:05 +0530

    SUBMISSION-1

---
+++ flip.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+	    $('.flipWrapper').click(function () {
+
+	        $(this).find('.card').toggleClass('flipped');
+
+	        return false;
+
+	    });
+
+	});
